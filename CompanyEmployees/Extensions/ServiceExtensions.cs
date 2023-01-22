af3a2c9ab29bb6cb5eff188e1c9acdfacb3f9b5a
Raphael Iyin <RaphaelLyin@MacBook-Pro.lan>
2023-01-21 19:00:00 -0500

    initial services set up

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CompanyEmployees
+{
+    public static class ServiceExtensions
+    {
+        // MARK: - Cross-Origin Resources Sharing (CORS)
+        /// a mechanism to give or restrict access rights to applications from different domains
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) => services.Configure<IISOptions>(options =>
+        {
+        });
+     }
+}
+
